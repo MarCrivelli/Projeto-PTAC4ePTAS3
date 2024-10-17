55a39e0d35305afa0ecb234079e6d3d2c6f008af
MarCrivelli <131816938+MarCrivelli@users.noreply.github.com>
2024-10-17 17:33:32 -0400

    Update 7
    Adicionado um ícone e umas funções que ainda não estão sendo utilizadas.

--- src/app/componentes/paginaCadastro/cadastro.tsx
+++ src/app/componentes/paginaCadastro/cadastro.tsx
@@ -1,9 +1,35 @@
+'use client'
 import Image from "next/image";
-import style from "./login.module.css";
+import style from "./cadastro.module.css";
 import Header from "../header/header";
 import Footer from "../footer/footer";
+import { useState } from "react";
+import Usuario from "@/app/interfaces/usuario";
+import { useRouter } from "next/router";
+import React from 'react';
+import { FaQuestion } from 'react-icons/fa';
 
-export default function Login() {
+export default function Cadastro() {
+
+  const [email, setEmail] = useState<string>()
+
+  const [senha, setSenha] = useState<string>()
+
+  const [usuario, setUsuario] = useState<Usuario>({
+    nome: '',
+    email: '',
+    password: ''
+  })
+
+  const alterarNome = (novoNome: string) => (
+    setUsuario((valoresAnteriores) =>({
+      ...valoresAnteriores,
+      nome: novoNome
+    })),
+    console.log(usuario)
+  )
+
+
   return (
     <div className={style.conteudoDoSite}>
         <Header/>        
@@ -16,6 +42,11 @@ export default function Login() {
                       <input type="email" className={style.inputLogin}></input>
                       <label>Digite sua senha</label>
                       <input type="password" className={style.inputLogin}></input>
+                      <label>Digite seu nome</label>
+                      <input type="name" className={style.inputLogin}></input>
+                      <div className={style.ajusteAjuda}>
+                        <FaQuestion className={style.iconeAjuda} size={24} />
+                      </div>
                     </div>
                     <button className={style.botaoLogin}>
                       Cadastrar
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import style from "./page.module.css";
-import Login from "./componentes/paginaLogin/login";
+import Login from "./componentes/paginaCadastro/cadastro";
 import Perfil from "./perfil/page";
 
 export default function Home() {
