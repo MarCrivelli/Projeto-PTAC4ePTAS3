99183c9a404204155d45e65739cb5afed6e1803d
MarCrivelli <marcoantonio7crivelli@gmail.com>
2024-11-14 03:00:22 -0300

    Update 18
    Estou fazendo a parte de PTAS do zero para entender o código e tentar fazer ele obter o resultado esperado.

--- back/index.js
+++ back/index.js
@@ -1,13 +1,21 @@
-const { PrismaCLient } = require("@prisma/client");
-	const prisma = new PrismaClient();
-	
-	const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const prisma = new PrismaClient();
 
-	const app = express();
-	app.use(express.json());
-	
-	//rotas
-	const authRoutes = require("./routes/authRoutes");
-	app.use("/auth", authRoutes);
-	
-	app.listen(8000);
+//Função que, ao ser chamada, vai criar um usuário e trazê-lo já cadastrado.
+async function main() {
+    //O ".usuário" se trata do nome da tabela que foi criada.
+    const usuario = await prisma.usuario.create({
+        data:{
+            nome: "Marco",
+            email: "aeeerawr@exemplo.com",
+            password: "123",
+            tipo: "cliente",
+        },
+    });
+    //Função para buscar todos os usuários.
+    const usuarios = await prisma.usuario.findMany();
+    console.log("Usuários: " + JSON.stringify(usuarios))
+    console.log("Novo Usuário: " + JSON.stringify(usuario));
+}
+
+main();
