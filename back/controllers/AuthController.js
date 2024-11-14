640ae228d5a915d6230345a62d8053caba3600b2
MarCrivelli <marcoantonio7crivelli@gmail.com>
2024-11-14 04:07:17 -0300

    Update 20
    Salvando o progresso até a terceira vídeo-aula de PTAS.

--- back/controllers/AuthController.js
+++ back/controllers/AuthController.js
@@ -4,7 +4,19 @@ const prisma = new PrismaClient();
 class AuthController {
 
     static async cadastro(req, res){
+        const{nome, email, password, tipo} = req.body;
 
+        if(!nome || nome.length < 6){
+            return res.json({
+                erro: true,
+                mensagem: "O nome deve ter 6 caracteres.",
+            });
+        }
+        return res.json({
+            erro: false,
+            mensagem: "Usuário cadastrado!.",
+            token: "12asdasd",
+        });
     }
 
     static async login(req, res){
